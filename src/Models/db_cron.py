b0e98604bd7998def03e75841c81607abd7e68aa
Maya Gershovitz <mgershovitz@gmail.com>
2020-09-07 20:30:34 +0300

    Create db_cron.py

---
+++ src/Models/db_cron.py
@@ -0,0 +1,10 @@
+# Once a day, run a job, go to folder/s3/etc... look for new files (against db table of data files) and update db
+
+class dbCron()
+  def __init__():
+    self.cron_interval = 24 hours
+    self.data_files_collection_name
+    self.db
+    
+  def load_from_csv_to_db():
+    pass
