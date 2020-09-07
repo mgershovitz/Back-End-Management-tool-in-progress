83349a69f0ae15d16e0bce8905c0580a1f3f357f
Maya Gershovitz <mgershovitz@gmail.com>
2020-09-07 20:17:44 +0300

    Remove explicit secret key

--- src/Models/__init__.py
+++ src/Models/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '3c690aa66f07182f1e510c3b2af275e0exit()'
+app.config['SECRET_KEY'] = os.getEnvVar('SECRET_KEY')
 
 from src.Models import routes
 from src.Models import utils
